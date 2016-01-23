7e5e66422c493b0812d36aa19bef62b3cb4efef3
Sloganathans <sabharinathan0603@gmail.com>
2016-01-23 05:06:34 -0800

    rtt22r

--- js/age.js
+++
@@ -1,9 +0,0 @@
-
-    function calculateAge()
-    {
-    var bday=parseInt(document.forms[0].year.value);
-    var now = new Date();
-    tage=(now.getFullYear());
-    alert("you are:"+(tage-bday)+ " years old");
-}
-
